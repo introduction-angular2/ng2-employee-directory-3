52ca46e3d8f939f6bcdc0a8f078db549efceb43e
Iyyasamy Srinivasan, Kumaran X. -ND <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2016-01-18 17:31:18 -0500

    Intial commit

---
+++ app/boot.ts
@@ -0,0 +1,5 @@
+import {bootstrap}     from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {AppRouter} from './app.router';
+
+bootstrap(AppRouter, [ROUTER_PROVIDERS]);
